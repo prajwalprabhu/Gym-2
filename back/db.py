77f836ab790f7b7e660887ad04449ad74081c038
T Prajwal Prabhu <prajwalprabhu.tellar@gmail.com>
2024-11-18 20:44:31 +0530

    db cahnges

--- back/db.py
+++ back/db.py
@@ -1,12 +1,16 @@
+import os
+
 from loguru import logger
 from sqlalchemy import create_engine, text
-import os
+
 # env is render then use different db
-mode = 'local'
-connection_url = "postgresql://gym_w4s6_user:RVjtnRCmHmmeeq1mcMIHsLbskC0SDTv4@dpg-cstl2n8gph6c739g7l4g-a.oregon-postgres.render.com/gym_w4s6" if mode!='local' else "postgresql://postgres:amin123@localhost:5432/gym"
-db = create_engine(
-    connection_url
+mode = "local" if os.environ.get("RENDER") is None else "render"
+connection_url = (
+    "postgresql://gym_w4s6_user:RVjtnRCmHmmeeq1mcMIHsLbskC0SDTv4@dpg-cstl2n8gph6c739g7l4g-a/gym_w4s6"
+    if mode != "local"
+    else "postgresql://postgres:amin123@localhost:5432/gym"
 )
+db = create_engine(connection_url)
 
 
 def get_db():
@@ -21,9 +25,7 @@ def get_db():
     except Exception as e:
         logger.error(e)
         logger.info("Reconnecting")
-        db = create_engine(
-            connection_url
-        )
+        db = create_engine(connection_url)
         conn = db.connect()
         logger.info("Reconnected")
         yield conn
